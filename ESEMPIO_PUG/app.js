d1ad995edf67133ca6a114ba3dd42e123f862638
imbroinise99 <imbroinise.davide@iisgalvanimi.edu.it>
2019-10-24 11:58:51 +0000

    esempiopug

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,14 +1,18 @@
-var express = require('express');
-var app = express();
 
-app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
 
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
- //res.send('Ciao Mondo');
- res.render('index');  //Dico a express di processare e inviare la pagina index.pug
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
 });
+
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
 });
